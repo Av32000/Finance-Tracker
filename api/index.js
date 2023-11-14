6389561c3b26a29342c70558054967e9123bd24b
Av32000 <ae.av32000@gmail.com>
2023-11-14 13:32:06 +0100

    Switch to Fastify

--- api/index.js
+++ api/index.js
@@ -1,166 +1,193 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const fastify = require("fastify")({
+  logger: false,
+});
+const cors = require("@fastify/cors");
+const multipart = require("@fastify/multipart");
+const util = require("util");
 const path = require("path");
 const AccountsAPI = require("./AccountsAPI");
-const { upload, filePath } = require("./upload");
-const { existsSync } = require("fs");
+const { existsSync, createWriteStream, createReadStream, readFileSync } = require("fs");
+const { pipeline } = require("stream");
+const { randomUUID } = require("crypto");
 
-const app = express();
+const filesPath = "datas/files/";
+if (!existsSync(filesPath)) mkdirSync(filesPath);
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
+fastify.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "DELETE", "PATCH"],
+  allowedHeaders: "Content-Type, Authorization"
 });
 
-// TODO : Add Passkey Auth
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname,"../", filesPath),
+})
 
+fastify.register(multipart);
+
+const pump = util.promisify(pipeline);
+
+// TODO : Add Passkey Auth
 const accountsPath = path.join(__dirname, "../", "datas");
 const accountsAPI = new AccountsAPI(accountsPath);
 accountsAPI.FixAccounts();
-accountsAPI.CleanFile(filePath);
+accountsAPI.CleanFile(filesPath);
 
 const port = parseInt(
   process.argv.find((s) => s.startsWith("--port"))?.split("=")[1] || "3000"
 );
 
-app.get("/", (req, res) => {
-  res.send("Finance Tracker!");
+fastify.get("/", async () => {
+  return "Finance Tracker !";
 });
 
 // Account
-app.get("/accounts", (req, res) => {
-  res.send(JSON.stringify(accountsAPI.GetAccounts()));
+fastify.get("/accounts", async () => {
+  return accountsAPI.GetAccounts();
 });
 
 // TODO : Switch to /accounts
-app.get("/account/:id", (req, res) => {
-  if (!req.params.id) return res.sendStatus(400);
-  res.send(JSON.stringify(accountsAPI.GetAccount(req.params.id)));
+fastify.get("/account/:id", async (request, reply) => {
+  return accountsAPI.GetAccount(request.params.id);
 });
 
-app.post("/accounts", (req, res) => {
-  const name = req.body.name;
-  if (!name) return res.sendStatus(400);
-  res.send(accountsAPI.CreateAccount(name));
+fastify.post("/accounts", async (request, reply) => {
+  const name = request.body.name;
+  if (!name) throw new Error("No name found");
+  return accountsAPI.CreateAccount(name);
 });
 
-app.patch("/accounts/:id", (req, res) => {
-  const id = req.params.id;
-  const newName = req.body.name;
-  if (!id || !newName) return res.sendStatus(400);
+fastify.patch("/accounts/:id", async (request, reply) => {
+  const id = request.params.id;
+  const newName = request.body.name;
+  if (!id || !newName) throw new Error("No ID or NewName found");
   accountsAPI.RenameAccount(id, newName);
-  res.sendStatus(200);
+  reply.status(200);
 });
 
-app.delete("/accounts/:id", (req, res) => {
-  const id = req.params.id;
-  if (!id) return res.sendStatus(400);
+fastify.delete("/accounts/:id", async (request, reply) => {
+  const id = request.params.id;
   accountsAPI.DeleteAccount(id);
-  res.sendStatus(200);
+  reply.status(200);
 });
 
 // Transaction
-app.get("/accounts/:accountId/transactions", (req, res) => {
-  const accountId = req.params.accountId;
-  if (!accountId) return res.sendStatus(400);
-  res.send(JSON.stringify(accountsAPI.GetTransactions(accountId)));
+fastify.get("/accounts/:accountId/transactions", async (request, reply) => {
+  const accountId = request.params.accountId;
+  return accountsAPI.GetTransactions(accountId);
 });
 
-app.get("/accounts/:accountId/transactions/:transactionId", (req, res) => {
-  const accountId = req.params.accountId;
-  const transactionId = req.params.transactionId;
-  if (!accountId || !transactionId) return res.sendStatus(400);
-  res.send(
-    JSON.stringify(accountsAPI.GetTransaction(accountId, transactionId))
-  );
-});
+fastify.get(
+  "/accounts/:accountId/transactions/:transactionId",
+  async (request, reply) => {
+    const accountId = request.params.accountId;
+    const transactionId = request.params.transactionId;
+    return accountsAPI.GetTransaction(accountId, transactionId);
+  }
+);
 
-app.post("/accounts/:accountId/transactions", (req, res) => {
-  const accountId = req.params.accountId;
-  const name = req.body.name;
-  const amount = req.body.amount;
-  const date = req.body.date;
-  const tag = req.body.tag;
-  const file = req.body.file;
+fastify.post("/accounts/:accountId/transactions", async (request, reply) => {
+  const accountId = request.params.accountId;
+  const name = request.body.name;
+  const amount = request.body.amount;
+  const date = request.body.date;
+  const tag = request.body.tag;
+  const file = request.body.file;
 
   if (!accountId || !name || !amount || !date || !tag)
-    return res.sendStatus(400);
-  if (file && (!file.id || !file.name)) return res.sendStatus(400);
-  res.send(
-    accountsAPI.AddTransaction(accountId, name, amount, date, tag, file)
-  );
+    throw new Error("Required field not found");
+  if (file && (!file.id || !file.name)) throw new Error("File not found");
+  return accountsAPI.AddTransaction(accountId, name, amount, date, tag, file);
 });
 
-app.patch("/accounts/:accountId/transactions/:transactionId", (req, res) => {
-  const accountId = req.params.accountId;
-  const transactionId = req.params.transactionId;
-  if (!accountId || !transactionId) return res.sendStatus(400);
-  accountsAPI.PatchTransaction(accountId, transactionId, req.body);
-  res.sendStatus(200);
-});
+fastify.patch(
+  "/accounts/:accountId/transactions/:transactionId",
+  async (request, reply) => {
+    const accountId = request.params.accountId;
+    const transactionId = request.params.transactionId;
+    accountsAPI.PatchTransaction(accountId, transactionId, req.body);
+    reply.status(200);
+  }
+);
 
-app.delete("/accounts/:accountId/transactions/:transactionId", (req, res) => {
-  const accountId = req.params.accountId;
-  const transactionId = req.params.transactionId;
-  if (!accountId || !transactionId) return res.sendStatus(400);
-  accountsAPI.DeleteTransaction(accountId, transactionId);
-  res.sendStatus(200);
-});
+fastify.delete(
+  "/accounts/:accountId/transactions/:transactionId",
+  async (request, reply) => {
+    const accountId = request.params.accountId;
+    const transactionId = request.params.transactionId;
+    accountsAPI.DeleteTransaction(accountId, transactionId);
+    reply.status(200);
+  }
+);
 
 // Files
-app.get("/files/:file", (req, res) => {
-  let file = req.params.file;
+fastify.get("/files/:file", async (request, reply) => {
+  let file = request.params.file;
   file = file.replace(/[\\/]/g, "");
-  let fullPath = path.join(__dirname, "../", filePath, file);
-  if (!file || !existsSync(fullPath)) return res.sendStatus(400);
-  res.sendFile(fullPath);
+  let fullPath = path.join(__dirname, "../", filesPath, file);
+  console.log(fullPath);
+  if (!file || !existsSync(fullPath)) throw new Error("File not found");
+  const fileContent = readFileSync(fullPath);
+
+  reply.type('text/plain').send(fileContent);
 });
 
 // Monthly
-app.patch("/accounts/:accountId/monthly", (req, res) => {
-  const accountId = req.params.accountId;
-  const newMonthly = req.body.monthly;
-  if (!accountId || !newMonthly || typeof newMonthly !== 'number') return res.sendStatus(400);
-  accountsAPI.SetMonthly(accountId, newMonthly)
-  res.sendStatus(200)
+fastify.patch("/accounts/:accountId/monthly", async (request, reply) => {
+  const accountId = request.params.accountId;
+  const newMonthly = request.body.monthly;
+  if (!accountId || !newMonthly || typeof newMonthly !== "number")
+    throw new Error("Invalid newMonthly");
+  accountsAPI.SetMonthly(accountId, newMonthly);
+  reply.status(200);
 });
 
 // Settigns
-app.get("/accounts/:accountId/settings", (req, res) => {
-  const accountId = req.params.accountId;
-  if (!accountId) return res.sendStatus(400);
+fastify.get("/accounts/:accountId/settings", async (request, reply) => {
+  const accountId = request.params.accountId;
   const account = accountsAPI.GetAccount(accountId);
-  if (!account) return res.sendStatus(400);
-  res.send(JSON.stringify(account.settings));
+  if (!account) return reply.status(400);
+  return account.settings;
 });
 
-app.get("/accounts/:accountId/settings/:setting", (req, res) => {
-  const accountId = req.params.accountId;
-  const settingName = req.params.setting;
-  if (!accountId || settingName) return res.sendStatus(400);
-  const account = accountsAPI.GetAccount(accountId);
-  if (!account) return res.sendStatus(400);
-  const setting = account.settings.find((s) => s.name === settingName);
-  if (!setting) return res.sendStatus(400);
-  res.send(JSON.stringify(setting));
-});
+fastify.get(
+  "/accounts/:accountId/settings/:setting",
+  async (request, reply) => {
+    const accountId = request.params.accountId;
+    const settingName = request.params.setting;
+    const account = accountsAPI.GetAccount(accountId);
+    if (!account) return reply.status(400);
+    const setting = account.settings.find((s) => s.name === settingName);
+    if (!setting) return reply.status(400);
+    return setting;
+  }
+);
 
-app.post("/accounts/:accountId/settings/", (req, res) => {
-  const accountId = req.params.accountId;
-  const newSetting = req.body;
+fastify.post("/accounts/:accountId/settings", async (request, reply) => {
+  const accountId = request.params.accountId;
+  const newSetting = request.body;
   if (!accountId || !newSetting || !newSetting.name || !newSetting.value)
-    return res.sendStatus(400);
+    throw new Error("Invalid Setting");
   accountsAPI.SetSetting(accountId, newSetting);
-  res.sendStatus(200);
+  reply.status(200);
 });
 
-app.post("/files/upload", upload.single("file"), (req, res) => {
-  res.send(req.file.filename.split(".")[0]);
+fastify.post("/files/upload", async (request, reply) => {
+  const data = await request.file();
+  const newName = randomUUID()
+  await pump(
+    data.file,
+    createWriteStream(
+      path.join(filesPath,newName + "." + data.filename.split(".").pop())
+    )
+  );
+  return newName;
 });
 
-app.listen(port, () => {
-  console.log(`Finance Tracker API listening on port ${port}`);
+fastify.listen({ port, host: "localhost" }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+  console.log(`Finance Tracker API listening on ${address}`);
 });
